847a710a2d4dec5fab0f981989f9a286d9cf3298
Jared-Godar <jared.godar@gmail.com>
2022-06-09 21:28:24 -0500

    Update Athena sql queries

--- athena_create_table.sql
+++ athena_create_table.sql
@@ -1,6 +1,4 @@
--- Create Athena Table - BOOTCAMP_CUSTOMERS_<EMP_ID>
--- Replace relevant placeholders in the template to match your values
--- Remove these comments before running DDL on Athena's Query Editor
+-- Create Athena Table
 
 CREATE EXTERNAL TABLE capstone_venues_478003 
 (
@@ -19,3 +17,27 @@ ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 STORED AS TEXTFILE
 LOCATION 's3://capstone-s3-478003/datalake/venues/duck'
 TBLPROPERTIES ('has_encrypted_data'='true', 'skip.header.line.count'='1');
+
+-- Create partitions
+
+ALTER TABLE capstone_venues_478003
+ADD PARTITION (venues_folder = 'duck') 
+  LOCATION 's3://capstone-s3-478003/datalake/venues/duck/';
+
+ALTER TABLE capstone_venues_478003 
+ADD PARTITION (venues_folder = 'heights') 
+  LOCATION 's3://capstone-s3-478003/datalake/venues/heights/';
+
+ALTER TABLE capstone_venues_478003
+ADD PARTITION (venues_folder = 'white_oak') 
+  LOCATION 's3://capstone-s3-478003/datalake/venues/white_oak/';
+  
+ALTER TABLE capstone_venues_478003
+ADD PARTITION (venues_folder = 'warehouse_live') 
+  LOCATION 's3://capstone-s3-478003/datalake/venues/warehouse_live/';
+
+-- Sample Queries
+
+SELECT * FROM capstone_venues_478003
+  WHERE price < 25 AND
+  DATE < 
